dd818ec83e7c883d1ac7693a2e637106c1a53ce0
xi 3 <168142156+bjjmartin@users.noreply.github.com>
2025-04-22 20:38:05 -0300

    aula 1
    primeira aula.

---
+++ chat-bd3-aula-2025-2-main/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const http = require('http');
+const app = express();
+const server = http.createServer(app);
+const ejs = require('ejs');
+const path = require("path");
+
+
+app.use(express.static(path.join(__dirname, 'public' )));
+
+app.set('view', path.join(__dirname, 'public' ));
+
+app.engine('html', ejs.renderFile);
+
+app.use('/', (req, resp) => {
+    resp.render('index.html');
+});
+
+server.listen(3000, ()=>{console.log("CHAT RODANDO EM - http://localhost:3000")});
